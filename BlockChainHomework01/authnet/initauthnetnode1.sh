cb819e233fd5d64fb1ab0fcf89081e01c5a60a65
nahom80 <nahom80@yahoo.com>
2020-05-20 15:22:49 -0400

    Add files via upload

---
+++ BlockChainHomework01/authnet/initauthnetnode1.sh
@@ -0,0 +1,3 @@
+cd "$HOME\OneDrive\FinTechBootCamp\BlockChainHomework01\authnet"
+geth init authnet.json --datadir node1
+read -p "Press <ENTER> to close"
---
+++ BlockChainHomework01/authnet/initauthnetnode2.sh
@@ -0,0 +1,3 @@
+cd "$HOME\OneDrive\FinTechBootCamp\BlockChainHomework01\authnet"
+geth init authnet.json --datadir node2
+read -p "Press <ENTER> to close"
